54d7aa49f7edc0f775fa9cfa534416b04d4e26e2
charlescheng091703 <charlescheng2024@u.northwestern.edu>
2022-06-16 11:31:33 -0500

    Implement state machine

--- receiver/receive.py
+++ receiver/receive.py
@@ -1,16 +1,23 @@
+# Author: Charles Cheng
+# Modified: 06/16/2022
+
+import time, json
 import serial
-import time
-import json
 from zfec.easyfec import Decoder
 
+# Define constants
 RECEIVE_PORT_NAME = 'COM3' # mode
-BAUDRATE = 230400
+BAUDRATE = 230400 # must agree with that of sender
 RECEIVE_DELAY = 1e-4 # rate at which serial buffer is being copied to local memory
 BUFFER_SIZE = 2147483647 # decrease if buffer overflows
 NUM_TOTAL_BLOCKS = 25
 NUM_RECONSTR_BLOCKS = 20 # NUM_TOTAL_BLOCKS - NUM_RECONSTR_BLOCKS = number of check blocks
-TIMEOUT_TIME = 15 # maximum time permitted per file transfer
+TIMEOUT_TIME = 2 # time to wait before end of transmission is assumed
+STATE = "IDLE" # ACTIVE when data is being sent or timeout has not occurred
+               # IDLE when file is being written or timeout has occurred
 
+# Takes a json-formatted dictionary containing filename and file contents
+# and writes a new file 
 def write_file(data):
     data_dict = json.loads(data)
     with open(data_dict["filename"], 'w', encoding='utf-8') as file:
@@ -18,36 +25,46 @@ def write_file(data):
     print(data_dict["filename"]+" successfully transmitted.")
     return data_dict["filename"] # returns filename
 
-def receive_data():
-    buffer = b''
+# Performs zfec decoding
+# If successful, it writes the file with the received data
+# Otherwise, it prints an error statement 
+def reconstruct_and_write(buffer):
     dec = Decoder(NUM_RECONSTR_BLOCKS, NUM_TOTAL_BLOCKS)
+    block_size = int(len(buffer)/NUM_RECONSTR_BLOCKS)
+    blocks = [] # list of package contents
+    blocknums = [] # list of block indices
+    try:
+        for i in range(NUM_RECONSTR_BLOCKS):
+            # First three digits are block number, the rest of bytes are package content
+            blocknums.append(int(buffer[block_size*i:block_size*i+3].decode()))
+            blocks.append(buffer[block_size*i+3:block_size*(i+1)])
+        decoded = dec.decode(blocks, sharenums=blocknums, padlen=0).decode().rstrip('\0')
+        filename = write_file(decoded)
+    except:
+        print("Invalid transmission received. Unable to process.")
+
+def receive_data():
+    global STATE
+    buffer = b''
+    timeout = time.time()
     with serial.Serial(RECEIVE_PORT_NAME, baudrate=BAUDRATE, parity=serial.PARITY_ODD) as port:
         port.set_buffer_size(rx_size=BUFFER_SIZE, tx_size=BUFFER_SIZE)
-        while True:
-            timeout = time.time()
-            while (time.time() - timeout < TIMEOUT_TIME):
+        while True: # runs infinitely; Ctrl + C to quit
+            # Implementation of state machine 
+            if STATE == "ACTIVE":
                 if port.inWaiting():
-                    try:
-                        buffer += port.read(port.inWaiting())
-                    except serial.SerialException:
-                        print("Serial port transmit/receive error detected.")
-                time.sleep(RECEIVE_DELAY)
-            buffer_length = len(buffer)
-            if buffer_length != 0:
-                block_size = int(buffer_length/NUM_RECONSTR_BLOCKS)
-                blocks = []
-                blocknums = [] 
-                try:
-                    for i in range(NUM_RECONSTR_BLOCKS):
-                        blocknums.append(int(buffer[block_size*i:block_size*i+3].decode()))
-                        blocks.append(buffer[block_size*i+3:block_size*(i+1)])
-                    decoded = dec.decode(blocks, sharenums=blocknums, padlen=0).decode().rstrip('\0')
-                    filename = write_file(decoded)
-                except:
-                    print("Invalid transmission received. Unable to process.")
-                buffer = b''
-            else:
-                print("Serial port timeout. No files received.")
+                    timeout = time.time()
+                    buffer += port.read(port.inWaiting())
+                    time.sleep(RECEIVE_DELAY)
+                elif time.time() - timeout > TIMEOUT_TIME:
+                    STATE = "IDLE"
+            elif STATE == "IDLE":
+                buffer_length = len(buffer)
+                if buffer_length != 0:
+                    reconstruct_and_write(buffer)
+                    buffer = b''
+                elif port.inWaiting():
+                    STATE = "ACTIVE"
 
 if __name__ == '__main__':
     receive_data()
--- sender/send.py
+++ sender/send.py
@@ -1,17 +1,21 @@
+# Author: Charles Cheng
+# Modified: 06/16/2022
+
+import time, json, random
 import serial
 import click
-import time
-import json
 from zfec.easyfec import Encoder
-import random
 
+# Define constants
 TRANSMIT_PORT_NAME = '/dev/tty.usbserial-14420' # ls /dev/tty.*
-BAUDRATE = 230400  
+BAUDRATE = 230400 # must agree with that of receiver 
 BUFFER_SIZE = 2147483647 # decrease if buffer overflows
 TRANSMIT_DELAY = 1 # increase if buffer overflows
 NUM_TOTAL_BLOCKS = 25
 NUM_RECONSTR_BLOCKS = 20 # NUM_TOTAL_BLOCKS - NUM_RECONSTR_BLOCKS = number of check blocks
 
+# Converts file to json format: filename (metadata) and file contents stored
+# Returns (unicode) encoded dictionary
 def file_to_json(filename):
     file = open(filename, 'r')
     msg = file.read()
@@ -20,19 +24,30 @@ def file_to_json(filename):
     data_json = json.dumps(data_dict)
     return data_json.encode()
 
+# Performs zfec encoding 
+# For more information about encoding parameterization,
+# visit https://pypi.org/project/zfec/ 
+def zfec_encode(data):
+    enc = Encoder(NUM_RECONSTR_BLOCKS, NUM_TOTAL_BLOCKS)
+    stream = enc.encode(data)
+    # Randomly chooses which NUM_RECONSTR_BLOCKS packages to send
+    packet_constrs = random.sample(list(enumerate(stream)), NUM_RECONSTR_BLOCKS)
+
+    buffer_out = b''
+    for constr in packet_constrs:
+        # Each package/block starts with 3 digits denoting the block number (metadata)
+        # followed by the contents of the package
+        buffer_out += f"{constr[0]:03}".encode() + constr[1]
+    return buffer_out
+
 @click.command()
 @click.option('--name', prompt='Name of file to send', help='Name of file to send.')
 def send_file(name):
     with serial.Serial(TRANSMIT_PORT_NAME, baudrate=BAUDRATE, parity=serial.PARITY_ODD) as port:
         msg = file_to_json(name)
-        enc = Encoder(NUM_RECONSTR_BLOCKS, NUM_TOTAL_BLOCKS)
-        stream = enc.encode(msg)
-        packet_constrs = random.sample(list(enumerate(stream)), NUM_RECONSTR_BLOCKS)
+        buffer_out = zfec_encode(msg)
 
-        buffer_out = b''
-        for constr in packet_constrs:
-            buffer_out += f"{constr[0]:03}".encode() + constr[1]
-
+        # Ensures maximum size of data sent out at once is equal to BUFFER_SIZE
         for i in range(0, len(buffer_out), BUFFER_SIZE):
             port.write(buffer_out[i:min(i+BUFFER_SIZE, len(buffer_out))])
             time.sleep(TRANSMIT_DELAY)
